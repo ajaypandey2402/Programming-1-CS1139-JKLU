2ba461933a1674a687b9fb4ae963568385b2e28b
Ajay Kumar Pandey <ajaypandey2402@gmail.com>
2026-08-26 22:23:43 +0530

    Refactor code with comments for better understanding
    Updated comments and structured code for clarity.

--- lab4_1-7_AKP.py
+++ lab4_1-7_AKP.py
@@ -1,5 +1,8 @@
 '''
 #1
+#A company decided to give bonus of 5% to employee if his/her year of service is more than 5 years.
+#Ask user for their salary and years of service and print the net bonus amount.
+
 salary = int(input('Enter the amount of salary: '))
 years = int(input('Enter the amount of years: '))
 if years > 5:
@@ -10,15 +13,29 @@ else:
 '''   
 '''
 #2
+#Write a program that reads input data from the console - a password (one line of random text)
+#and checks if the input matches the phrase "s3cr3t!P@ssw0rd". If it matches, print "Welcome",
+#otherwise print "Wrong password!".
+
 password = input('Enter the password: ')
 if password == 's3cr3t!P@ssw0rd':
     print('Welcome')
 else:
     print('Wrong password')
 '''
-
-#3
 '''
+#3
+#Write a program that reads input data from the console - the measures of a geometric
+#shape and calculates its area. There are four types of
+#shapes: square, rectangle, circle and triangle.
+#The first line of input is the type of shape (square, rectangle, circle, triangle):
+#If the shape is a square, the next argument will be one number - the length of its side.
+#If the shape is a rectangle, the next argument will be two numbers - the lengths of its sides.
+#If the shape is a circle, the next argument will be one number - the radius of the circle.
+#If the shape is a triangle, the next argument will be two numbers - its base and the
+#corresponding altitude.
+#The result should be rounded up to the third decimal point.
+
 shape = input('Input the type of shape: ')
 if shape == 'square':
     length = int(input('Input the length: '))
@@ -43,6 +60,13 @@ else:
 '''
 '''
 #4
+#Write a program that reads two integers - hours and minutes based on a 24-hour day format and
+#calculates what time it will be 15 minutes later. The result should be printed in the following
+#format hh:mm. Hours should always be between 0 and 23, while minutes should always be
+#between 0 and 59. Hours should be written with one or two digits as needed, while the minutes
+#should always be written with two digits - add a leading zero, as needed. Example, Given the time
+#1:46 as input, the output displayed should be 2:01.
+
 h = int(input('Enter hours: '))
 m = int(input('Enter minutes: '))
 if (m < 45):
@@ -59,6 +83,13 @@ else:
 '''
 '''
 #5
+#A student has to travel n kilometers. He can choose between three types of transportation:
+#Taxi. Starting fee: 33.58 INR. Day rate: 37.89 INR/km. Night rate: 43.17 INR/km.
+#Bus. Day / Night rate: 4.32 INR/km. Can be used for distances of a minimum of 20 km.
+#Train. Day / Night rate: 2.88 INR/km. Can be used for distances of a minimum of 100 km.
+#Write a program that reads the number of kilometers n and period of the day (day or night) and
+#calculates the price for the cheapest transport.
+
 n = int(input('Enter the number of kilometers: '))
 day = input('Enter the period of the day: ')
 if day == 'day':
@@ -89,8 +120,17 @@ elif n >= 20:
 else:
     print('The cheapest transport price is:', taxi)
 '''
-#6
 '''
+#6
+#Tom Cat likes to sleep all day but, unfortunately, his owner is always playing with him whenever
+#he has free time. To sleep well, the norm of games that Tom has is 30,000 minutes per year. The
+#time for games he has depends on the holidays that his owner has:
+#• During workdays, his owner plays with him 63 minutes per day.
+#• During holidays, his owner plays with him 127 minutes per day.
+#Write a program that reads the number of holidays and prints whether Tom can sleep well and
+#how much the difference from the norm for the current year is. It is assumed that there are 365
+#days in one year.
+
 holidays = int(input('Enter the number of holidays: '))
 workdays = 365 - holidays
 playtime = workdays*63 + holidays*127
@@ -101,6 +141,26 @@ else:
 '''
 '''
 #7
+#Most of the people start planning their vacations well in advance. A young programmer from
+#Bulgaria has a certain budget (BGN is the currency of Bulgaria) and spare time in a particular
+#season.
+#Write a program that accepts as input the budget and season and as output displays
+#programmer's vacation place and the amount of money they will spend.
+#The budget determines the destination, and the season determines what amount of the budget
+#will be spent. If the season is summer, the programmer will go camping, if it is winter – he will stay
+#in a hotel. If it is in Europe, regardless of the season, the programmer will stay in a hotel. Each
+#camp or hotel, according to the destination, has its price, which corresponds to a particular
+#percentage of the budget:
+#• If 100 BGN or less – somewhere in Bulgaria.
+#Summer – 30% of the budget
+#Winter – 70% of the budget.
+#• If 1000 BGN or less – somewhere in the Balkans.
+#Summer – 40% of the budget.
+#Winter – 80% of the budget.
+#• If more than 1000 BGN – somewhere in Europe.
+#Upon traveling in Europe, regardless of the season, the programmer will
+#spend 90% of the budget.
+
 budget = int(input('Enter the budget in BGN: '))
 season = input('Enter the season: ')
 if budget > 1000:
